d8680c0022297e962b2feaf54f3e9a07e0846193
Vũ Thành Công <104372944+vuthanhcong02@users.noreply.github.com>
2023-05-28 21:59:56 +0700

    define methods ArticleController

---
+++ app/controllers/ArticleController.php
@@ -0,0 +1,41 @@
+<?php
+require_once 'app/services/ArticleService.php';
+require_once 'app/services/CategoryService.php';
+class ArticleController{
+    public function index(){
+        $articleService = new ArticleService();
+        $articles=$articleService->getAllArticle();
+        return $articles;
+    }
+    public function add(){
+        $title =$_POST['title'];
+        $summary =$_POST['summary'];
+        $category_id =$_POST['category_id'];
+        $articleService = new ArticleService();
+        $articleService->addArticle($title,$summary,$category_id);
+        header('Location: index.php?app/controller=home&action=index');
+    }
+    public function del(){
+        $id =$_GET['id'];
+        $articleService = new ArticleService();
+        $articleService->delArticle($id);
+        header('Location: index.php?controller=home&action=index');
+    }
+    public function edit(){
+        $id=$_GET['id'];
+        $articleService = new ArticleService();
+        $article = $articleService->editArticle($id);
+        $categoryService =new CategoryService();
+        $categories = $categoryService->getAllCategory();
+        include 'app/views/article/edit_article.php';
+    }
+    public function update(){
+        $id=$_POST['id'];
+        $title =$_POST['title'];
+        $summary =$_POST['summary'];
+        $category_id =$_POST['category_id'];
+        $articleService = new ArticleService();
+        $articleService->updateArticle($id,$title,$summary,$category_id);
+        header('Location: index.php?controller=home&action=index');
+    }
+}
