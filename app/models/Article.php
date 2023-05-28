b6f1034a0d2906495749b1475dd8c8d1463240bf
Vũ Thành Công <104372944+vuthanhcong02@users.noreply.github.com>
2023-05-28 21:51:03 +0700

    define model aricle and category

---
+++ app/models/Article.php
@@ -0,0 +1,48 @@
+<?php
+require_once 'database/Connect.php';
+class Article{
+    private $id;
+    private $title;
+    private $summary;
+    private $category_id;
+    private $category_name;
+    public function __construct($id,$title,$summary,$category_id,$category_name)
+    {
+        $this->id=$id;
+        $this->title=$title;
+        $this->summary=$summary;
+        $this->category_id=$category_id;
+        $this->category_name = $category_name;
+    }
+    public function getId(){
+        return $this->id;
+    }
+    public function setId($id){
+        $this->id=$id;
+    }
+    public function getTitle(){
+        return $this->title;
+    }
+    public function setTitle($title){
+        $this->title=$title;
+    }
+    public function getSummary(){
+        return $this->summary;
+    }
+    public function setSummary($summary){
+        $this->summary=$summary;
+    }
+    public function getCategory_id(){
+        return $this->category_id;
+    }
+    public function setCategory_id($category_id){
+        $this->category_id=$category_id;
+    }
+
+    public function getCategory_name(){
+        return $this->category_name;
+    }
+    public function setCategory_name($category_name){
+        $this->category_name=$category_name;
+    }
+}
---
+++ app/models/Category.php
@@ -0,0 +1,27 @@
+<?php
+require_once 'database/Connect.php';
+class Category{
+    private $id;
+    private $name;
+    public function __construct($id,$name){
+        $this->id = $id;
+        $this->name = $name;
+    }
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+
+}
